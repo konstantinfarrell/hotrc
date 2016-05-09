c68f6ae54182593c1f546265c35812cd2be92b2c
konstantinfarrell <konstantinvonmolotov@gmail.com>
2016-05-09 15:49:28 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '0.0.1'
+
+description = 'A command line tool for managing aliases in your .bashrc file.'
+current_dir = os.path.dirname(__file__)
+try:
+    long_description = open(os.path.join(cur_dir, 'README.md')).read()
+except:
+    long_description = description
+
+setup(
+    name = 'HotRC',
+    version = version,
+    packages = find_packages('src'),
+    url = 'https://github.com/konstantinfarrell/hotrc',
+    license = 'BSD',
+    description = description,
+    long_description = long_description,
+    author = 'Konstantin Farrell',
+    author_email = 'konstantinfarrell@gmail.com',
+    package_dir = {'': 'src'},
+    install_requires = ['setuptools'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ]
+)
