2b4d3ab8da02ceadb3deb562ab15c5bd0822a3f7
KonstantinFarrell <konstantinfarrell@gmail.com>
2016-07-15 13:09:33 -0700

    Add real test

--- tests/test_hotrc.py
+++ tests/test_hotrc.py
@@ -1,6 +1,22 @@
+import os
 from unittest import TestCase
+
+from hotrc.hotrc import HotRC
 
 
 class TestHotRC(TestCase):
-    def test_foo(self):
-        self.assertTrue(True)
+    hotrc = None
+
+
+    @classmethod
+    def setUpClass(cls):
+        cls.rcfile = '{}/.testrc'.format(os.path.dirname(__file__))
+        open(cls.rcfile, 'w+').close()
+        cls.hotrc = HotRC(bashrc=cls.rcfile)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.rcfile)
+
+    def test_rcfile(self):
+        self.assertTrue(os.path.isfile(self.hotrc.BASHRC))
